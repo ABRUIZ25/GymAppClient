f28ded99310a24a9a267bb2f04699a553ca30ef4
ABRUIZ25 <89878760+ABRUIZ25@users.noreply.github.com>
2022-04-27 12:52:18 -0400

    client updated to mongo

--- src/App.js
+++ src/App.js
@@ -1,30 +1,54 @@
-import Login from './Login'
-import SighUp from './SignUp'
-import './App.css';
+import { useState } from "react";
 
-import { Router, Route, Switch } from 'react-router-dom';
+import "./App.css";
+
+let serverUrl = "http://localhost:8080";
+
+if (process.env.REACT_APP_ENVIRONMENT === "production") {
+  console.log("in prod");
+  serverUrl = "https://final-workotserver.herokuapp.com/";
+} else if (process.env.REACT_APP_ENVIRONMENT === "development") {
+  console.log("in dev");
+  serverUrl = "http://localhost:8080";
+}
+
+const handleClickMe = async () => {
+  const response = await fetch(`${serverUrl}`);
+  const data = await response.json();
+  console.log("front end data ", data);
+  return data;
+};
 
 function App() {
-
+  const [message, setMessage] = useState("before");
   return (
-
     <div className="App">
       <header className="App-header">
+        {message}
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
 
-        <div className="Gym-name">
-          <h2>
-            Gym Name
-          </h2>
-        </div>
 
-        <div className="Login/Sign-Up">
-          <button>Login</button>
+        </a>
 
-        </div>
+        <button
+          onClick={async () => {
+            const fetchedMessage = await handleClickMe();
+            console.log("fetchedMessage ", fetchedMessage)
+            setMessage(fetchedMessage.message)
+          }}
+        >
+          Click me!
+        </button>
 
       </header>
-    </div>
 
+    </div>
   );
 }
 
